c5e9c9e06e1a90dea29df9519c87b25c61c3c45a
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-02 22:11:30 +0800

    init project for learning android

---
+++ app/src/main/java/com/example/tinymoon/activitytest/FirstActivity.java
@@ -0,0 +1,56 @@
+package com.example.tinymoon.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clieked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+
+        Button button1 = (Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(FirstActivity.this,"You clicked Button 1",
+//                        Toast.LENGTH_SHORT).show();
+//                finish();
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                startActivity(intent);
+                Intent intent = new Intent("com.example.tinymoon.activitytest.ACTION_START");
+                intent.addCategory("com.example.tinymoon.activitytest.MY_CATEGORY");
+                startActivity(intent);
+            }
+        });
+    }
+}
