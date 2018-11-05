04b92e0e3beb04c29b267700c539a6f6e6a94f49
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-05 19:39:50 +0800

    finished Activity Test

--- app/src/main/java/com/example/tinymoon/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/tinymoon/activitytest/FirstActivity.java
@@ -2,8 +2,10 @@ package com.example.tinymoon.activitytest;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -43,19 +45,51 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                1
 //                Toast.makeText(FirstActivity.this,"You clicked Button 1",
 //                        Toast.LENGTH_SHORT).show();
+
+//                2
 //                finish();
-//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+
+//                3
+//                Intent intent = new Intent(FirstActivity.this,
+//                        SecondActivity.class);
 //                startActivity(intent);
+
+//                4
 //                Intent intent = new Intent("com.example.tinymoon.activitytest.ACTION_START");
 //                intent.addCategory("com.example.tinymoon.activitytest.MY_CATEGORY");
 //                startActivity(intent);
 
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:10086"));
-                startActivity(intent);
+//                5
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+//                startActivity(intent);
+
+//                6
+//                String data = "Hello SecondActivity";
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+//                intent.putExtra("extra_data",data);
+//                startActivity(intent);
+
+//                7
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String resturnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity",resturnedData);
+                }
+                break;
+                default:
+        }
     }
 }
--- app/src/main/java/com/example/tinymoon/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/tinymoon/activitytest/SecondActivity.java
@@ -1,13 +1,43 @@
 package com.example.tinymoon.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+//        1
+//        Intent intent = getIntent();
+//        String data = intent.getStringExtra("extra_data");
+//        Log.d(TAG, data);
+
+        Button button2 = (Button)findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return","Hello FirstActivity");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return","Hello FirstActivity");
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
